1ab70e1d51578b03eab2929fc4f1accce58b57bb
sergiuscain <167008320+sergiuscain@users.noreply.github.com>
2024-09-23 12:38:20 +0300

    Добавил конструктор продукту.

--- Models/Products.cs
+++ Models/Products.cs
@@ -2,6 +2,7 @@
 {
     internal class Products
     {
+        int instanceCounter = 0;
         public int Id { get;}   //уникальный идентификатор
         public string Name { get;}  //название товара
         public string Description { get;} //описание товара
@@ -10,5 +11,18 @@
         public decimal Cost { get;} //себестоимость
         public string ImageUrl { get;} //патч изображения товара
         public string Category { get;} //категория товара
+
+        public Products(string name, string description, decimal price, int quantity, decimal cost, string imageUrl, string category)
+        {
+            this.Id = instanceCounter;
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Cost = cost;
+            this.ImageUrl = imageUrl;
+            this.Category = category;
+            instanceCounter++;
+        }
     }
 }
