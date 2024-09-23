06d9007e4283a51d41b903e15679b2a89d4d2955
sergiuscain <167008320+sergiuscain@users.noreply.github.com>
2024-09-23 12:30:01 +0300

    Добавил комментарии

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VseTShirts.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Index(int id)
+        {
+           
+            string result = $"";    
+            return result;
+        }
+    }
+}
--- Models/Products.cs
+++ Models/Products.cs
@@ -2,13 +2,13 @@
 {
     internal class Products
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
-        public string ImageUrl { get; set; }
-        public string Category { get; set; }
-        public string price { get; set; }
+        public int Id { get; set; }   //уникальный идентификатор
+        public string Name { get; set; }  //название товара
+        public string Description { get; set; } //описание товара
+        public decimal Price { get; set; }  //цена
+        public int Quantity { get; set; } //количество
+        public decimal Cost { get; set; } //себестоимость
+        public string ImageUrl { get; set; } //патч изображения товара
+        public string Category { get; set; } //категория товара
     }
 }
