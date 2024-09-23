ad6f37176eb187a5473169c70a9045d4a5a3b646
sergiuscain <167008320+sergiuscain@users.noreply.github.com>
2024-09-23 12:32:41 +0300

    Убрал сеттеры

--- Models/Products.cs
+++ Models/Products.cs
@@ -2,13 +2,13 @@
 {
     internal class Products
     {
-        public int Id { get; set; }   //уникальный идентификатор
-        public string Name { get; set; }  //название товара
-        public string Description { get; set; } //описание товара
-        public decimal Price { get; set; }  //цена
-        public int Quantity { get; set; } //количество
-        public decimal Cost { get; set; } //себестоимость
-        public string ImageUrl { get; set; } //патч изображения товара
-        public string Category { get; set; } //категория товара
+        public int Id { get;}   //уникальный идентификатор
+        public string Name { get;}  //название товара
+        public string Description { get;} //описание товара
+        public decimal Price { get;}  //цена
+        public int Quantity { get;} //количество
+        public decimal Cost { get;} //себестоимость
+        public string ImageUrl { get;} //патч изображения товара
+        public string Category { get;} //категория товара
     }
 }
