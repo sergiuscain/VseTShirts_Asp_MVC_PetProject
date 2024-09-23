e38474edbc7308f61201f5355626113273065266
sergiuscain <167008320+sergiuscain@users.noreply.github.com>
2024-09-23 10:12:03 +0300

    Выполнил задание 2.

---
+++ Controllers/calculator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VseTShirts.Controllers
+{
+    public class calculator : Controller
+    {
+        public string plus(int a, int b)
+        {
+
+            return $"{a} + {b} =  {a+b}";
+        }
+    }
+}
