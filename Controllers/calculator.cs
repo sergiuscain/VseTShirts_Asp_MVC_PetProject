e0fd4bb2e2352ba2f499620af27b630d60ce1993
sergiuscain <167008320+sergiuscain@users.noreply.github.com>
2024-09-23 10:25:33 +0300

    Задание 2.4

--- Controllers/calculator.cs
+++ Controllers/calculator.cs
@@ -4,7 +4,7 @@ namespace VseTShirts.Controllers
 {
     public class calculator : Controller
     {
-        public string index(int a, int b, char c)
+        public string index(float a, float b, char c)
         {
             if (c ==  '+')
             {
@@ -17,6 +17,13 @@ namespace VseTShirts.Controllers
             if (c == '*')
             {
                 return $"{a} * {b} =  {a * b}";
+            }
+            if (c == '/')
+            {
+                if (b == 0)
+                    return "На ноль делить нельзя";
+                else
+                    return $"{a} / {b} = {a/b}";
             }
             return "Неверное значение";
         }
