796cbfe89d5bc3ddd076228e098023ceae27a00a
sergiuscain <167008320+sergiuscain@users.noreply.github.com>
2024-10-01 16:09:00 +0300

    Совпадение паролей

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -1,16 +1,24 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace VseTShirts.Models
 {
     public class RegisterModel
     {
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string UserName { get; set; }
-
+        [Required]
         public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают!")]
         public string ConfirmPassword { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
     }
 }
