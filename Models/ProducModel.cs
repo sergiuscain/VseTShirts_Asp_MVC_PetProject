86ce895400cac3ebf423be878ce201205f81ce99
sergiuscain <167008320+sergiuscain@users.noreply.github.com>
2024-10-01 17:10:48 +0300

    Работал над валидацией данных через Asp.Net

--- Models/ProducModel.cs
+++ Models/ProducModel.cs
@@ -1,19 +1,48 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace VseTShirts.Models
 {
     public  class ProductModel
     {
         private static int instanceCounter = 0;
         public int Id { get;}   //уникальный идентификатор
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [MinLength(4, ErrorMessage = "Слишком короткое название")]
         public string Name { get; set; }  //название товара
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [MinLength(10, ErrorMessage = "Слишком короткое описание")]
         public string Description { get; set; } //описание товара
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Range(100,99000,ErrorMessage = "Некорректная цена")]
         public decimal Price { get; set; }  //цена
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Range(1,10000,ErrorMessage = "Некорректное количество товара")]
         public int Quantity { get; set; } //количество
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Range(50,99999,ErrorMessage = "Некорректная цена")]
         public decimal Cost { get; set; } //себестоимость
+
+        [Required(ErrorMessage = "Обязательное поле")]
         public string ImagePath { get; set; } //патч изображения товара
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [MinLength(4,ErrorMessage = "Слишком короткое название категрии")]
         public string Category { get; set; } //категория товара
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [MinLength(4, ErrorMessage = "Неправильное название цвета")]
         public string Color { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [MaxLength(4, ErrorMessage = "Введите размер одежды(XS/X/M/L/XL/XXL/XXXL)")]
         public string Size { get; set; }
+
 
         public static Product ToProduct(ProductModel model)
         {
--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -6,19 +6,25 @@ namespace VseTShirts.Models
 {
     public class RegisterModel
     {
-        [Required]
+        [Required(ErrorMessage ="Обязательное поле")]
+        [MinLength(8,ErrorMessage = "Email не может быть короче 8 символов")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Обязательное поле")]
+        [MinLength(6,ErrorMessage = "Логин не может быть короче 6 символов")]
         public string UserName { get; set; }
-        [Required]
+        [MinLength(6,ErrorMessage = "Пароль не может быть короче 6 символов")]
+        [Required(ErrorMessage ="Обязательное поле")]
         public string Password { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Обязательное поле")]
         [Compare("Password", ErrorMessage = "Пароли не совпадают!")]
+        [MinLength(6,ErrorMessage = "Пароль не может быть короче 6 символов")]
         public string ConfirmPassword { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Обязательное поле")]
+        [MinLength(10,ErrorMessage = "ФИО не может быть короче 10 символов")]
         public string Name { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Обязательное поле")]
+        [MinLength(8,ErrorMessage = "Номер телефона не может быть короче 9 символов")]
         public string PhoneNumber { get; set; }
     }
 }
